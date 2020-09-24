98528fdefdbaecac3fbe38ad9203052b6b464f73
Aparnasuman <Aparnasuman2@gmail.com>
2020-09-25 01:13:58 +0530

    container of bootstrap form making

---
+++ script.js
@@ -0,0 +1,9 @@
+var text="a,b,c,d,e";
+var x=text.split(" ").join();
+console.log(x);
+
+const myString = ' Hello-1-word-Sentence-number-2 ';
+const splits = myString.split("-");
+const nm=splits.join();
+
+console.log(nm);
