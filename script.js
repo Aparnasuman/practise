b605327dbdbec47b338ed09363981fcdd836b418
Aparnasuman <Aparnasuman2@gmail.com>
2020-09-25 13:34:05 +0530

    changs

--- script.js
+++
@@ -1,9 +0,0 @@
-var text="a,b,c,d,e";
-var x=text.split(" ").join();
-console.log(x);
-
-const myString = ' Hello-1-word-Sentence-number-2 ';
-const splits = myString.split("-");
-const nm=splits.join();
-
-console.log(nm);
